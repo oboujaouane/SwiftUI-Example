6f623883e38a4c0f86084b7e4f70d94c1e8e1d82
Ousama boujaouane <oboujaouane@gmail.com>
2020-11-07 23:30:51 +0100

    Initial Commit

---
+++ SwiftUI Example/SwiftUI_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ExampleApp.swift
+//  SwiftUI Example
+//
+//  Created by ousama boujaouane on 07/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
